6f870ea44c8a6889745442e5b080fef461816735
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2025-10-31 23:32:34 +0700

    fix: query categories by search

--- QuizApi/Repositories/CategoryRepository.cs
+++ QuizApi/Repositories/CategoryRepository.cs
@@ -41,6 +41,13 @@ namespace QuizApi.Repositories
                 .Where(x => x.RecordStatus.ToLower().Equals(RecordStatusConstant.Active.ToLower()))
                 .AsQueryable();
 
+            #region Query
+            if (!string.IsNullOrWhiteSpace(searchRequest.Search))
+            {
+                listCategoryQuery = listCategoryQuery.Where(x => EF.Functions.ILike(x.Name, $"%{searchRequest.Search}%"));
+            }
+            #endregion
+
             #region Ordering
             string orderBy = searchRequest.OrderBy;
             string orderDir = searchRequest.OrderDir;
