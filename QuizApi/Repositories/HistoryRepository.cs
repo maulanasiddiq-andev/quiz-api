b7e9022dbcde1a3d6f0ca6ecfb20dd61c21e864b
maulanasiddiq-andev <maulanasiddiqdeveloper@gmail.com>
2026-03-27 12:59:49 +0700

    fix: quiz history by id

--- QuizApi/Repositories/HistoryRepository.cs
+++ QuizApi/Repositories/HistoryRepository.cs
@@ -1,37 +1,67 @@
 using System.Linq.Expressions;
 using AutoMapper;
-using Microsoft.EntityFrameworkCore;
+using Google.Cloud.Firestore;
 using QuizApi.Constants;
 using QuizApi.DTOs.QuizHistory;
 using QuizApi.Exceptions;
-using QuizApi.Models;
 using QuizApi.Models.QuizHistory;
 
 namespace QuizApi.Repositories
 {
     public class HistoryRepository
     {
-        private readonly QuizAppDBContext dBContext;
+        private readonly FirestoreDb firestoreDb;
         private readonly IMapper mapper;
-        public HistoryRepository(QuizAppDBContext dBContext, IMapper mapper)
+        public HistoryRepository(
+            [FromKeyedServices("quiz-db")] FirestoreDb firestoreDb,
+            IMapper mapper
+        )
         {
-            this.dBContext = dBContext;
+            this.firestoreDb = firestoreDb;
             this.mapper = mapper;
         }
 
         public async Task<QuizHistoryDto> GetDataByIdAsync(string id)
         {
-            QuizHistoryModel? quizHistory = await dBContext.QuizHistory
-                .Where(x => x.QuizHistoryId.Equals(id) && x.RecordStatus == RecordStatusConstant.Active)
-                .Select(MapQuizHistoryWithQuestions)
-                .FirstOrDefaultAsync();
+            // 1. Fetch the Quiz Document
+            Query query = firestoreDb.Collection("quizhistory")
+                .WhereEqualTo("QuizHistoryId", id)
+                .WhereEqualTo("RecordStatus", RecordStatusConstant.Active)
+                .Limit(1);
 
-            if (quizHistory == null)
+            QuerySnapshot snapshot = await query.GetSnapshotAsync();
+
+            if (snapshot.Documents.Count == 0)
             {
                 throw new KnownException(ErrorMessageConstant.DataNotFound);
             }
 
+            var quizHistory = snapshot.Documents[0].ConvertTo<QuizHistoryModel>();
+
+            // 3. Fetch Active Questions for this Quiz
+            Query questionsQuery = firestoreDb.Collection("questionhistory")
+                .WhereEqualTo("QuizHistoryId", id)
+                .OrderBy("QuestionOrder");
+
+            QuerySnapshot questionsSnap = await questionsQuery.GetSnapshotAsync();
+            var questions = questionsSnap.Documents.Select(d => d.ConvertTo<QuestionHistoryModel>()).ToList();
+
+            // 4. Fetch Answers for each Question
+            // (Note: If you have many questions, consider storing answers as a nested array in the Question document)
+            foreach (var question in questions)
+            {
+                Query answersQuery = firestoreDb.Collection("answerhistory")
+                    .WhereEqualTo("QuestionHistoryId", question.QuestionHistoryId)
+                    .OrderBy("AnswerOrder");
+
+                QuerySnapshot answersSnap = await answersQuery.GetSnapshotAsync();
+                question.Answers = answersSnap.Documents.Select(d => d.ConvertTo<AnswerHistoryModel>()).ToList();
+            }
+
+            // 5. Map to DTO
+            quizHistory.Questions = questions;
             QuizHistoryDto quizHistoryDto = mapper.Map<QuizHistoryDto>(quizHistory);
+            quizHistoryDto.QuestionCount = quizHistory.Questions.Count;
 
             return quizHistoryDto;
         }
